2cc4cc5cce2cad3f97268ed3573e291fbdfe65ef
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2024-04-15 08:45:16 -0300

    Update Home.html
    estruturas e suas posições, hover de cada estrutura

--- home.js
+++ home.js
@@ -1,5 +1,23 @@
-function overlayHover() {
-    if(document.getElementById("firstArea").onmouseover == true) {
-        document.getElementById("overlay").style.opacity = 1;
+function overlayHover(idArea) {
+    if (idArea == "firstArea") {
+        document.getElementById("firstArea").style.zIndex = 10;
+        document.getElementById("eticaTitle").style.display = "flex";
+    } else if (idArea == "secondArea") {
+        document.getElementById("secondArea").style.zIndex = 10;
+        document.getElementById("economiaTitle").style.display = "flex";
+    } else if (idArea == "thirdArea") {
+        document.getElementById("thirdArea").style.zIndex = 10;
+        document.getElementById("guerraTitle").style.display = "flex";
     }
+    document.getElementById("overlay").style.opacity = 1;
+}
+
+function overlayHoverOff() {
+    document.getElementById("firstArea").style.zIndex = 0;
+    document.getElementById("secondArea").style.zIndex = 0;
+    document.getElementById("thirdArea").style.zIndex = 0;
+    document.getElementById("eticaTitle").style.display = "none";
+    document.getElementById("economiaTitle").style.display = "none";
+    document.getElementById("guerraTitle").style.display = "none";
+    document.getElementById("overlay").style.opacity = 0;
 }

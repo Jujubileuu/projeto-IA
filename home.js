0d164582da634d7bd742e0078383a5de80171255
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2024-04-17 21:16:10 -0300

    Update home.html
    Adição das animações

--- home.js
+++ home.js
@@ -2,14 +2,18 @@ function overlayHover(idArea) {
     if (idArea == "firstArea") {
         document.getElementById("firstArea").style.zIndex = 10;
         document.getElementById("eticaTitle").style.display = "flex";
+        document.getElementById("eticaTitle").classList.add("animatedFadeIn");
     } else if (idArea == "secondArea") {
         document.getElementById("secondArea").style.zIndex = 10;
         document.getElementById("economiaTitle").style.display = "flex";
+        document.getElementById("economiaTitle").classList.add("animatedFadeIn");
     } else if (idArea == "thirdArea") {
         document.getElementById("thirdArea").style.zIndex = 10;
         document.getElementById("guerraTitle").style.display = "flex";
+        document.getElementById("guerraTitle").classList.add("animatedFadeIn");
     }
     document.getElementById("overlay").style.opacity = 1;
+    document.getElementById("overlay").classList.add("animatedFadeIn");
 }
 
 function overlayHoverOff() {
@@ -20,4 +24,12 @@ function overlayHoverOff() {
     document.getElementById("economiaTitle").style.display = "none";
     document.getElementById("guerraTitle").style.display = "none";
     document.getElementById("overlay").style.opacity = 0;
+    document.getElementById("overlay").classList.remove("animatedFadeIn");
+}
+
+function openForm(){
+    document.getElementById("popUp").style.display = "block";
+}
+function closeForm(){
+    document.getElementById("popUp").style.display = "none";
 }

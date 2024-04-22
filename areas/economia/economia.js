b94a5cb54584b427644486785820eee55840a9e2
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2024-04-22 08:52:43 -0300

    Update economia.js
    Receios adicionados (2/5)

--- areas/economia/economia.js
+++ areas/economia/economia.js
@@ -19,14 +19,16 @@ function changeContent(idCard){
         document.getElementById("popUpText").innerHTML = "teste2";
         document.getElementById("popUpImg").src = "https://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png";
     } else if(idCard == "3") {
-        document.getElementById("popUpText").innerHTML = "teste3";
-        document.getElementById("popUpImg").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs5yf9d35OUjrxlQIpIhXop9rc9DN749axHenYIPlMk-aAzQrRDy94Ciins7zcfZfhE6o&usqp=CAU";
+        document.getElementById("popUpTitle").innerHTML = "Anônimo";
+        document.getElementById("popUpText").innerHTML = "“Funcionários robôs não pagam impostos e reduzem os custos de mão de obra, com isso as empresas multinacionais vão voltar a seus países de origem o que provoca uma desindustrialização de países mais pobres.”";
+        document.getElementById("popUpImg").src = "/img/pessoas/Anonimo.jpg";
     } else if(idCard == "4") {
         document.getElementById("popUpText").innerHTML = "teste4";
         document.getElementById("popUpImg").src = "https://thumbs.dreamstime.com/b/imagem-bonita-da-silhueta-da-natureza-65000328.jpg";
     } else if(idCard == "5") {
-        document.getElementById("popUpText").innerHTML = "teste5";
-        document.getElementById("popUpImg").src = "https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png";
+        document.getElementById("popUpTitle").innerHTML = "Anônimo";
+        document.getElementById("popUpText").innerHTML = "“A automatização de certos serviços por inteligências artificiais vai acabar gerando desemprego em massa.”";
+        document.getElementById("popUpImg").src = "/img/pessoas/Anonimo.jpg";
     }
 }
 

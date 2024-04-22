56e28eb3852eaf2deea969813e3c20da67299f9f
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2024-04-22 08:45:50 -0300

    Update guerra.js
    Receios adicionados (2/5)

--- areas/guerra/guerra.js
+++ areas/guerra/guerra.js
@@ -16,14 +16,16 @@ function changeContent(idCard){
         document.getElementById("popUpText").innerHTML = "teste1";
         document.getElementById("popUpImg").src = "https://imgv3.fotor.com/images/side/astronaut-in-space-looking-up-at-the-stars-generated-by-Fotor-AI.jpg";
     } else if(idCard == "2") {
-        document.getElementById("popUpText").innerHTML = "teste2";
-        document.getElementById("popUpImg").src = "https://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png";
+        document.getElementById("popUpTitle").innerHTML = "Anônimo";
+        document.getElementById("popUpText").innerHTML = "“então, o que impediria os robôs de nos escravizarem ou de dizimarem a nossa espécie?”";
+        document.getElementById("popUpImg").src = "/img/pessoas/Anonimo.jpg";
     } else if(idCard == "3") {
         document.getElementById("popUpText").innerHTML = "teste3";
         document.getElementById("popUpImg").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs5yf9d35OUjrxlQIpIhXop9rc9DN749axHenYIPlMk-aAzQrRDy94Ciins7zcfZfhE6o&usqp=CAU";
     } else if(idCard == "4") {
-        document.getElementById("popUpText").innerHTML = "teste4";
-        document.getElementById("popUpImg").src = "https://thumbs.dreamstime.com/b/imagem-bonita-da-silhueta-da-natureza-65000328.jpg";
+        document.getElementById("popUpTitle").innerHTML = "Mark Bishop";
+        document.getElementById("popUpText").innerHTML = "“em meu papel como especialista em IA no Comitê Internacional para Controle de Armas de Robôs, estou particularmente preocupado com a potencial implantação de sistemas de armas robóticas que pode engajar militarmente sem intervenção humana [...]”";
+        document.getElementById("popUpImg").src = "/img/pessoas/MarkBishop.jpg";
     } else if(idCard == "5") {
         document.getElementById("popUpText").innerHTML = "teste5";
         document.getElementById("popUpImg").src = "https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png";
